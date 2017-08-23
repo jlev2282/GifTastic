ca0c1100f4aca16f7d6b528aa1701d1988b7f6bc
John <jlev2282@gmail.com>
2017-08-23 13:16:00 -0400

    changed gify address

--- gif.js
+++ gif.js
@@ -16,7 +16,7 @@ createButton(topics);
 
 	$(".animal-button").on("click", function() {
 
-	var queryURL ="http://api.giphy.com/v1/gifs/search?q="+this.id+"&api_key=dc6zaTOxFJmzC&limit=10";
+	var queryURL ="https://api.giphy.com/v1/gifs/search?q="+this.id+"&api_key=dc6zaTOxFJmzC&limit=10";
 
 		$.ajax({url:queryURL,
 				method: "GET"
